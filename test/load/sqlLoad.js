c946880365691dbe6d0c3730327f3350490774ce
Dongju Jung <lostcode7@users.noreply.github.com>
2026-07-29 01:26:53 +0900

    Remove v1 implementation
    Delete the callable singleton loader (index.js, lib/loader.js) and its
    assertion-less tests. Keep the original SQL files as test fixtures under
    test/fixtures/basic for the v2 test suite.

--- test/load/sqlLoad.js
+++
@@ -1,7 +0,0 @@
-module.exports.root_dir_test = function(){
-	return require("../../index.js")("/sql");
-}
-
-module.exports.caller_dir_test = function(){
-	return require("../../index.js")("../sql");
-}
